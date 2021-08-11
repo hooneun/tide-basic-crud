a8df541469c665b4146837ee80d86162ce995a26
kimhoon <kimhoon0316@gmail.com>
2021-08-11 21:32:20 +0900

    setting

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use tide::Request;
+use tide::prelude::*;
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    tide::log::start();
+    let mut app = tide::new();
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
