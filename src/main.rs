23389801f131c335e5d2abe79405a91c0dba6d8b
kimhoon <kimhoon0316@gmail.com>
2021-08-12 18:13:34 +0900

    async memory storage ing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use tide::Request;
+use tide::{Request, Response, Body};
 use tide::prelude::*;
+use std::collections::HashMap;
+use std::sync::{RwLock, Arc};
+
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Dino {
+    name: String,
+    weight: u16,
+    diet: String,
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    dinos: Arc<RwLock<HashMap<String, Dino>>>,
+}
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
-    let mut app = tide::new();
+    let state = State {
+        dinos: Default::default(),
+    };
+    let mut app = tide::with_state(state);
     app.at("/").get(|_| async { Ok("Hello, world!") });
+
+    app.at("/dinos").post(|mut req: Request<State>| async move {
+        let dino: Dino = req.body_json().await?;
+        let mut dinos = req.state().dinos.write().await;
+        dinos.insert(String::from(&dino.name), dino);
+        let mut res = Response::new(201);
+        res.set_body(Body::from_json(&dino)?);
+
+        Ok(res)
+    });
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
